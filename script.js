93f6c3e7fdb7b018beebec112081fe617ba96794
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-07-03 11:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const inputText = document.getElementById('text');
+const inputDelay = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
+  const text = inputText.value;
+  const delay = parseInt(inputDelay.value);
+  await new Promise(resolve => setTimeout(resolve, delay));
+outputDiv.textContent = text;
+}
+btn.addEventListener('click', () => {
+  displayMessageWithDelay();
+});
